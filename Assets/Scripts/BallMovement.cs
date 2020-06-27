25425230532bb57725d742de7334b3e2797b2580
klesouza <kle_zoio@hotmail.com>
2020-06-27 22:54:44 +0200

    kinda of working version

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rigidbody2D;
+    public float speed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Vector2 velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        rigidbody2D.velocity = velocity * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"Ball bounced {collision.collider.name}");
+
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,33 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class GameController : MonoBehaviour
 {
 
-    public GameObject player1;
-    public GameObject player2;
-    public GameObject boundaries;
-    public GameObject ball;
-    Vector3 lastPos;
+    public Rigidbody2D rb;
+    public float speed = 7f;
+    private float yPos;
     // Start is called before the first frame update
     void Start()
     {
-        lastPos = player1.transform.position;
-        //Debug.Log($"Boundaries: {boundaries.transform.localScale}");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xPos = Input.GetAxis("Horizontal");
-        float yPos = Input.GetAxis("Vertical");
-        ball.transform.Translate(Vector3.left * 3 * Time.deltaTime);
-        //if(player1.transform.position.y > boundaries.transform.localScale.y  )
-            player1.transform.Translate(new Vector3(xPos, yPos));
-        if (lastPos != player1.transform.position) 
-            Debug.Log($"New position {player1.transform.position}");
-        lastPos = player1.transform.position;
+        yPos = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(rb.velocity.x, yPos * speed);
+    }
+
+    private void FixedUpdate()
+    {
     }
 }
