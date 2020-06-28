056275595874f6ed71a42fb5f37415a71024519e
klesouza <kle_zoio@hotmail.com>
2020-06-28 18:49:58 +0200

    Adding simple auto play

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -7,11 +7,12 @@ public class BallMovement : MonoBehaviour
 
     public Rigidbody2D rigidbody2D;
     public float speed = 4f;
+    public float speedIncrease = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
 
-        Vector2 velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        Vector2 velocity = new Vector2(Random.Range(1f, 2f), Random.Range(1f, 2f));
         rigidbody2D.velocity = velocity * speed;
     }
 
@@ -23,11 +24,13 @@ public class BallMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if(Time.fixedDeltaTime % 6 == 0)
+            rigidbody2D.velocity *= (1+speedIncrease);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log($"Ball bounced {collision.collider.name}");
+        //Debug.Log($"Ball bounced {collision.collider.name}");
 
     }
 }
