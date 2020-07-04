6cfccd33a96ee826b89798d7d07db9964ab8450a
klesouza <kle_zoio@hotmail.com>
2020-07-04 16:33:53 +0200

    Adding Pause logic

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsGamePaused = false;
+    public GameObject pauseMenuUI;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            IsGamePaused = !IsGamePaused;
+            pauseMenuUI.SetActive(IsGamePaused);
+            Time.timeScale = IsGamePaused ? 0 : 1;
+        }
+    }
+}
